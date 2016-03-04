f684dcc5e20d90fb3d5eb749834683ad742766a7
Joshua Cramoy <jdcramoy@gmail.com>
2016-03-04 02:22:02 -0500

    mig-server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.set('port', (process.env.PORT || 5000));
 //app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
- response.send('HIT ME WITH SOME DATA');
+ response.send('HIT ME WITH SOME DATA, MEOW!!');
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
