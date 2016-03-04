5e493dc80d4285f70dc6c0acbe87a26649780e81
Joshua Cramoy <jdcramoy@gmail.com>
2016-03-03 22:12:43 -0500

    mig-server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var pg = require('pg');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
 
 
@@ -11,7 +11,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(express.bodyParser());
+//app.use(express.bodyParser());
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
