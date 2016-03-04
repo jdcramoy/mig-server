14526397b4f5ba04a3a3f57e2b0c12b1d8ec058e
Joshua Cramoy <jdcramoy@gmail.com>
2016-03-03 22:43:55 -0500

    mig-server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser = require("body-parser");
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
  response.send('HIT ME WITH SOME DATA');
