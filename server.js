ed95d8f7ebb83062c3338d090607f56830344cbc
Joshua Cramoy <jdcramoy@gmail.com>
2016-03-04 00:00:41 -0500

    mig-server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/', function(request, response){
 	    var hubid = request.body.hubid;
 		console.log(request.body.email);
 	    console.log(request.body.hubid);
-	    response.end("yes");
+	    response.end(request.body.hubid, request.body.email, "yes");
 
 
 }); 
