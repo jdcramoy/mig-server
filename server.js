eff170e905c5c195dba2cc570b17e54d5002fb7b
Joshua Cramoy <jdcramoy@gmail.com>
2016-03-03 22:30:00 -0500

    mig-server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+
 // include the http module
-var http = require('http');
+/*var http = require('http');
 var express = require('express');
 var app = express();
 var pg = require('pg');
@@ -26,7 +27,7 @@ app.get('/form', function(request, response) {
 /*app.post('/user/:email/:hubid', function(req, res) {
  console.log(req.params.email);
  console.log(req.params.hubid);
-}); */
+}); 
 //app.use(express.bodyParser());
 
 app.post('/form', function(request, response){
@@ -42,6 +43,46 @@ request.on('data', function(chunk) {
   body = Buffer.concat(body).toString();
   console.log(body);
   // at this point, `body` has the entire request body stored in it as a string
-});
+}); */
+
+var http = require('http');
+
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+    // BEGINNING OF NEW STUFF
+
+    response.on('error', function(err) {
+      console.error(err);
+    });
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.writeHead(200, {'Content-Type': 'application/json'})
+
+    var responseBody = {
+      headers: headers,
+      method: method,
+      url: url,
+      body: body
+    };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.end(JSON.stringify(responseBody))
+
+    // END OF NEW STUFF
+  });
+}).listen(8080);
 
 
