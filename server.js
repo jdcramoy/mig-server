8786c37f638ff68b8633a08862685c98ba16ea7e
Joshua Cramoy <jdcramoy@gmail.com>
2016-03-03 20:20:06 -0500

    mig-server

---
+++ server.js
@@ -0,0 +1,27 @@
+//Lets require/import the HTTP module
+var http = require('http');
+var dispatcher = require('httpdispatcher');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening");
+});
